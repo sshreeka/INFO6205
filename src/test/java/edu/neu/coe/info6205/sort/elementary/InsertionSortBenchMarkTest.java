75a42f7e99921e81e90b13017b7ba104ce308c51
Shreekara <ss.s@northeastern.edu>
2021-09-26 21:32:57 -0400

    java/edu/neu/coe/info6205/sort/elementary/InsertionSortBenchMarkTest.java  - Unit tests for insertion sort from repeat()
    Signed-off-by: Shreekara <ss.s@northeastern.edu>

--- src/test/java/edu/neu/coe/info6205/sort/elementary/InsertionSortBenchMarkTest.java
+++ src/test/java/edu/neu/coe/info6205/sort/elementary/InsertionSortBenchMarkTest.java
@@ -1,2 +1,24 @@
-package edu.neu.coe.info6205.sort.elementary;public class InsertionSortBenchMarkTest {
+package edu.neu.coe.info6205.sort.elementary;
+import edu.neu.coe.info6205.util.Timer;
+
+import java.util.Random;
+
+public class InsertionSortBenchMarkTest {
+    public static void main(String[] args) {
+        Random rd = new Random(); // creating Random object
+        Integer[] arr = new Integer[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rd.nextInt();} // storing random integers in an array
+        Timer timer = new Timer();
+        final double mean = timer.repeat(50, () -> arr, t -> {
+            InsertionSort.sort(t);
+            return null;
+        });
+        System.out.println(" Mean:" + mean);
+        System.out.println("Random numbers");
+        for (int i: arr){
+            System.out.println(i);
+        }
+
+    }
 }
